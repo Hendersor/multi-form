98b16463138c1e239e03f36280b135c587a24f73
Francisco <montejof1998@gmail.com>
2023-02-06 15:38:09 -0800

    I added functionality to the button next step

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 const nxtButton = document.querySelector(
   ".main-container .main-container__buttons .main-container__buttons-next"
 );
-
 nxtButton.addEventListener("click", showNextContainer);
 
+const bckButton = document.querySelector(
+  ".main-container__buttons .main-container__buttons-back"
+);
+bckButton.classList.add("visible");
+bckButton.addEventListener("click", showPreviousContainer);
+
+const nextStepContainer = document
+  .querySelector(".main-container__steps")
+  .querySelectorAll(".main-container__steps__steps--container");
+
+const mainContainer = document
+  .querySelector(".main-container")
+  .querySelectorAll("section");
+
 function showNextContainer() {
-  const mainContainer = document
-    .querySelector(".main-container")
-    .querySelectorAll("section");
+  bckButton.classList.remove("visible");
+  let actualStep;
+  let siblingStep;
+
+  nextStepContainer.forEach((container) => {
+    if (container.classList.contains("active")) {
+      actualStep = container;
+      siblingStep = actualStep.nextElementSibling;
+    }
+  });
+  actualStep.classList.remove("active");
+  siblingStep.classList.add("active");
 
   let actualContainer;
   let sibling;
@@ -15,6 +37,31 @@ function showNextContainer() {
     if (!container.classList.contains("visible")) {
       actualContainer = container;
       sibling = actualContainer.nextElementSibling;
+    }
+  });
+  actualContainer.classList.add("visible");
+  sibling.classList.remove("visible");
+}
+
+function showPreviousContainer() {
+  let actualStep;
+  let siblingStep;
+
+  nextStepContainer.forEach((container) => {
+    if (container.classList.contains("active")) {
+      actualStep = container;
+      siblingStep = actualStep.previousElementSibling;
+    }
+  });
+  actualStep.classList.remove("active");
+  siblingStep.classList.add("active");
+
+  let actualContainer;
+  let sibling;
+  mainContainer.forEach((container) => {
+    if (!container.classList.contains("visible")) {
+      actualContainer = container;
+      sibling = actualContainer.previousElementSibling;
     }
   });
   actualContainer.classList.add("visible");

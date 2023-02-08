bc580a335a1d2d10fc00e50266b490d553dffabd
Francisco <montejof1998@gmail.com>
2023-02-07 16:41:50 -0800

    the switch is fully working

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ const planContainer = document
   .querySelector(".main-container__plan__options")
   .querySelectorAll(".main-container__plan__options__container");
 
+//functionality for the sequence of numbers
 planContainer.forEach((container) => {
   container.addEventListener("click", () => {
     planContainer.forEach((i) => {
@@ -88,17 +89,56 @@ function showPreviousContainer() {
   sibling.classList.remove("visible");
 }
 
-// const chkContainer = document.querySelector(".main-container__plan__options__switch");
-// chkContainer
+//checkbox for plan selection
 const optionsContainer = document
   .querySelector(".main-container__plan")
   .querySelectorAll(".main-container__plan__options__container");
 
+const extrasContainer = document.querySelectorAll(
+  ".main-container__extras-container__picks__container"
+);
+
 const chk = document.getElementById("checkBox");
 chk.addEventListener("click", () => {
   optionsContainer.forEach((container) => {
+    const yearlyPrice = container
+      .querySelector(".main-container__plan__options__container__price")
+      .querySelectorAll(
+        ".main-container__plan__options__container__price__yearly"
+      );
+
+    const monthlyPrice = container
+      .querySelector(".main-container__plan__options__container__price")
+      .querySelectorAll(
+        ".main-container__plan__options__container__price__monthly"
+      );
+    monthlyPrice.forEach((price) => {
+      price.classList.toggle("visible");
+    });
+
+    yearlyPrice.forEach((price) => {
+      price.classList.toggle("visible");
+    });
     container.querySelectorAll("p").forEach((p) => {
       p.classList.toggle("visible");
     });
+  });
+
+  extrasContainer.forEach((extraContainer) => {
+    const priceMonthly = extraContainer
+      .querySelectorAll(
+        ".main-container__extras-container__picks__container-price__monthly"
+      )
+      .forEach((price) => {
+        price.classList.toggle("visible");
+      });
+
+    extraContainer
+      .querySelectorAll(
+        ".main-container__extras-container__picks__container-price__yearly"
+      )
+      .forEach((price) => {
+        price.classList.toggle("visible");
+      });
   });
 });
